b05990357f22d6ed623a7f2867eca322f0b5ec1b
lee jaejung <jjuiddong@gmail.com>
2016-11-26 00:32:17 +0900

    hello world

---
+++ Src/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "lee";
+            string lastName = "test";
+
+            //System.Console.WriteLine("Hello world {0} {1}", firstName,  lastName);
+            //System.Console.WriteLine( $"Hello world {firstName} { lastName }");
+            System.Console.WriteLine($"0x0{42:X}");
+        }
+    }
+}
